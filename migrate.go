0ba76d7115343d1149df06d6c5c2ff7cc6acb200
DeedleFake <deedlefake@users.noreply.github.com>
2022-04-27 23:01:05 -0400

    migrate: use a topological sort

--- migrate.go
+++ migrate.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"strings"
 
 	"deedles.dev/migrate/internal/util"
+	"golang.org/x/exp/slices"
 )
 
 // MigrationFunc is the signature matched by functions that define
@@ -27,22 +27,9 @@ func Plan(funcs map[string]MigrationFunc) (*Migration, error) {
 		verts[n] = &m
 	}
 
-	steps := make([]*M, 0, len(verts))
-	for _, m := range verts {
-		err := fillDeps(&m.deps, verts)
-		if err != nil {
-			return nil, fmt.Errorf("migration %q dependencies: %w", m.name, err)
-		}
-
-		steps = util.SortedInsertFunc(steps, m, func(v1, v2 *M) int {
-			if v2.deps.Contains(v1.name) {
-				return -1
-			}
-			if v1.deps.Contains(v2.name) {
-				return 1
-			}
-			return strings.Compare(v1.name, v2.name)
-		})
+	steps, err := flattenDAG(verts)
+	if err != nil {
+		return nil, fmt.Errorf("calculate migration order: %w", err)
 	}
 
 	return &Migration{
@@ -66,22 +53,56 @@ func (m *Migration) Steps() []string {
 	return steps
 }
 
-type sqler interface {
-	sql(Dialect) string
-}
+func flattenDAG(verts map[string]*M) (steps []*M, err error) {
+	defer func() {
+		switch r := recover().(type) {
+		case nil:
+		case error:
+			err = r
+		default:
+			panic(r)
+		}
+	}()
 
-func fillDeps(deps *util.Set[string], verts map[string]*M) error {
-	// TODO: Detect dependency cycles.
-	more := true
-	for more {
-		more = false
-		for _, dep := range deps.Slice() {
-			v, ok := verts[dep]
+	rem := util.SortedKeys(verts) // To ensure determinsitic behavior.
+	steps = make([]*M, 0, len(verts))
+
+	var perm util.Set[string]
+	var tmp util.Set[string]
+	var inner func(*M)
+	inner = func(m *M) {
+		if perm.Contains(m.name) {
+			return
+		}
+		defer perm.Add(m.name)
+
+		if tmp.Contains(m.name) {
+			panic(fmt.Errorf("dependency cycle detected at %q", m.name))
+		}
+		tmp.Add(m.name)
+		defer tmp.Remove(m.name)
+
+		deps := m.deps.Slice()
+		slices.Sort(deps) // To ensure deterministic behavior.
+		for _, dep := range deps {
+			d, ok := verts[dep]
 			if !ok {
-				return fmt.Errorf("no such migration: %q", dep)
+				panic(fmt.Errorf("migration %q depends on non-existent migration %q", m.name, dep))
 			}
-			more = deps.AddSet(v.deps) || more
+			inner(d)
+		}
+
+		steps = append(steps, m)
+	}
+
+	for perm.Len() < len(rem) {
+		for _, name := range rem {
+			inner(verts[name])
 		}
 	}
-	return nil
+	return steps, nil
+}
+
+type sqler interface {
+	sql(Dialect) string
 }
