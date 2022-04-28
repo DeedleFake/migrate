f54ac5733cfa7b0f583010d6c1548630088eb96c
DeedleFake <deedlefake@users.noreply.github.com>
2022-04-27 23:07:42 -0400

    migrate: add some more tests

--- migrate_test.go
+++ migrate_test.go
@@ -21,3 +21,23 @@ func TestPlan(t *testing.T) {
 	assert.NilError(t, err, "generate plan")
 	assert.DeepEqual(t, plan.Steps(), []string{"Start", "Left", "LeftLeft", "LeftRight", "Right", "End"})
 }
+
+func TestCyclicPlan(t *testing.T) {
+	funcs := map[string]migrate.MigrationFunc{
+		"A": func(m *migrate.M) { m.Require("C") },
+		"B": func(m *migrate.M) { m.Require("A") },
+		"C": func(m *migrate.M) { m.Require("B") },
+	}
+
+	_, err := migrate.Plan(funcs)
+	assert.ErrorContains(t, err, "dependency cycle detected at")
+}
+
+func TestNonExistentDependency(t *testing.T) {
+	funcs := map[string]migrate.MigrationFunc{
+		"A": func(m *migrate.M) { m.Require("Uh oh...") },
+	}
+
+	_, err := migrate.Plan(funcs)
+	assert.ErrorContains(t, err, "depends on non-existent migration")
+}
